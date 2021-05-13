c4380a744196564ceafc6fc3749a5f6fd707bb16
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-05-13 16:33:07 +0530

    Routes form validation added,

--- src/components/pages/AddBusRoutes.js
+++ src/components/pages/AddBusRoutes.js
@@ -1,26 +1,57 @@
 import React, { Component } from "react";
 
 export default class AddBusRoutes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { from: "", to: "", isFromValid: false, isToValid: false };
+  }
+
+  onFormSubmit = async (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  onFromChange = (e) => {
+    const value = e.target.value;
+    let isFromValid = false;
+    if (value) {
+      isFromValid = true;
+    }
+    this.setState((preState) => ({
+      ...preState,
+      from: e.target.value,
+      isFromValid,
+    }));
+  };
+
+  onToChange = (e) => {
+    const value = e.target.value;
+    let isToValid = false;
+    if (value) {
+      isToValid = true;
+    }
+    this.setState((preState) => ({
+      ...preState,
+      to: e.target.value,
+      isToValid,
+    }));
+  };
+
+  isFromValid = () => {
+    return this.state.isFromValid && this.state.isToValid;
+  };
+
   render() {
     return (
       <div className="card card-body my-3">
         <h3 className="text-capitalize text-center">Add Routes</h3>
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <div className="input-group">
             <div className="input-group-prepend">
-              <div className="input-group-text bg-primary text-white">
-                Bus ID
-              </div>
-            </div>
-            <input type="text" className="form-control" placeholder="Bus ID" />
-          </div>
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <div className="input-group-text bg-primary text-white">
-                Location 1
-              </div>
+              <div className="input-group-text bg-primary text-white">From</div>
             </div>
             <input
+              onChange={this.onFromChange}
               type="text"
               className="form-control"
               placeholder="Starting place"
@@ -28,19 +59,20 @@ export default class AddBusRoutes extends Component {
           </div>
           <div className="input-group">
             <div className="input-group-prepend">
-              <div className="input-group-text bg-primary text-white">
-                Location 2
-              </div>
+              <div className="input-group-text bg-primary text-white">To</div>
             </div>
             <input
+              onChange={this.onToChange}
               type="text"
               className="form-control"
               placeholder="Departure"
             />
           </div>
-          <button type="submit" className="btn btn-block btn-primary mt-3">
-            + Add Route
-          </button>
+          {this.isFromValid() && (
+            <button type="submit" className="btn btn-block btn-primary mt-3">
+              + Add Route
+            </button>
+          )}
         </form>
       </div>
     );
