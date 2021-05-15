a75cf7704394c4ee4d69f111391f8c1e8db6ee1a
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-05-15 08:49:15 +0530

    Added Schedule Screen

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import setAuthToken from "./util/setAuthToken";
 import Alert from "./components/layout/Alert";
 import BusRoute from "./components/pages/BusRoute";
 import BusTypes from "./components/pages/BusTypes";
+import Schedule from "./components/pages/Schedule";
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
           <Route exact path="/register" component={Register}></Route>
           <Route exact path="/bustypes" component={BusTypes}></Route>
           <Route exact path="/busroute" component={BusRoute}></Route>
+          <Route exact path="/schedule" component={Schedule}></Route>
         </Switch>
       </Router>
     </Provider>
