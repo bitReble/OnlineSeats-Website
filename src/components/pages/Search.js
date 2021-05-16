e0d55fa50c0e713697cc878de4b6a8ff0f5e51f9
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-05-16 15:50:07 +0530

    Showing schedule,

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { searchSchedules } from "../../actions/schedule";
 
-const Search = ({ searchSchedules }) => {
+const Search = ({ searchSchedules, schedules }) => {
   const now = new Date();
   const currentDate = `${now.getFullYear()}-${(now.getMonth() + 1)
     .toString()
@@ -29,79 +29,132 @@ const Search = ({ searchSchedules }) => {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-10 mx-auto col-md-8 mt-4">
-          <div className="card card-body my-3">
-            <h3 className="text-capitalize text-center">Search for schedule</h3>
-            <form style={{ marginTop: "20px" }} onSubmit={onFormSubmit}>
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <div className="input-group-text bg-primary text-white">
-                    From
+    <>
+      {!!schedules.length && (
+        <ul className="list-group my-5" style={{ padding: "20px" }}>
+          <h3 className="text-capitalize text-center">Schedule List</h3>
+          <table style={{ marginLeft: "20px", marginRight: "20px" }}>
+            <thead>
+              <tr className="list-group-item text-capitalize d-flex justify-content-between">
+                <th>Id</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Departure</th>
+                <th>Arrival</th>
+                <th>Time stamp</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {schedules.map((schedule, index) => {
+                return (
+                  <tr
+                    key={index}
+                    className="list-group-item text-capitalize d-flex justify-content-between"
+                  >
+                    <td>
+                      <h6>{`${schedule?._id.toString().substr(0, 7)} ...`}</h6>
+                    </td>
+                    <td>{schedule.route.path[0]}</td>
+                    <td>{schedule.route.path[1]}</td>
+                    <td>{schedule.departure}</td>
+                    <td>{schedule.arrival}</td>
+                    <td>{`${schedule.from
+                      .toString()
+                      .substr(0, 10)} To ${schedule.to
+                      .toString()
+                      .substr(0, 10)}`}</td>
+                    <td className="todo-icon">
+                      <span className="mx-2 text-success" onClick={() => {}}>
+                        <i className="fas fa-pen" />
+                      </span>
+                      <span className="mx-2 text-danger" onClick={() => {}}>
+                        <i className="fas fa-trash" />
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </ul>
+      )}
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <div className="card card-body my-3">
+              <h3 className="text-capitalize text-center">
+                Search for schedule
+              </h3>
+              <form style={{ marginTop: "20px" }} onSubmit={onFormSubmit}>
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text bg-primary text-white">
+                      From
+                    </div>
                   </div>
+                  <input
+                    onChange={(e) => {
+                      setState((preState) => {
+                        return { ...preState, from: e.target.value };
+                      });
+                    }}
+                    value={state.from}
+                    type="text"
+                    className="form-control"
+                    placeholder="Starting place"
+                  />
                 </div>
-                <input
-                  onChange={(e) => {
-                    setState((preState) => {
-                      return { ...preState, from: e.target.value };
-                    });
-                  }}
-                  value={state.from}
-                  type="text"
-                  className="form-control"
-                  placeholder="Starting place"
-                />
-              </div>
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <div className="input-group-text bg-primary text-white">
-                    To
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text bg-primary text-white">
+                      To
+                    </div>
                   </div>
+                  <input
+                    onChange={(e) => {
+                      setState((preState) => {
+                        return { ...preState, to: e.target.value };
+                      });
+                    }}
+                    value={state.to}
+                    type="text"
+                    className="form-control"
+                    placeholder="Departure"
+                  />
                 </div>
-                <input
-                  onChange={(e) => {
-                    setState((preState) => {
-                      return { ...preState, to: e.target.value };
-                    });
-                  }}
-                  value={state.to}
-                  type="text"
-                  className="form-control"
-                  placeholder="Departure"
-                />
-              </div>
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <div className="input-group-text bg-primary text-white">
-                    Date
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text bg-primary text-white">
+                      Date
+                    </div>
                   </div>
+                  <input
+                    onChange={(e) => {
+                      setState((preState) => {
+                        return { ...preState, date: e.target.value };
+                      });
+                    }}
+                    value={state.date}
+                    type="date"
+                    className="form-control"
+                    placeholder="Departure"
+                  />
                 </div>
-                <input
-                  onChange={(e) => {
-                    setState((preState) => {
-                      return { ...preState, date: e.target.value };
-                    });
-                  }}
-                  value={state.date}
-                  type="date"
-                  className="form-control"
-                  placeholder="Departure"
-                />
-              </div>
-              {isFormValid && (
-                <button
-                  type="submit"
-                  className="btn btn-block btn-primary mt-3"
-                >
-                  Search
-                </button>
-              )}
-            </form>
+                {isFormValid && (
+                  <button
+                    type="submit"
+                    className="btn btn-block btn-primary mt-3"
+                  >
+                    Search
+                  </button>
+                )}
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
