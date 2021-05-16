44d162b294db42a9b3b5f75a9a212432c85fbff6
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-05-16 22:54:30 +0530

    Schedule Screen UI changed

---
+++ src/actions/schedule.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+import { LOGOUT, SCHEDULE_FETCHED, SCHEDULE_CREATED } from "./types";
+
+export const getSchedules = () => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const now = new Date();
+  const body = JSON.stringify({
+    from: "Colombo",
+    to: "Sammanthurai",
+    date: now.setDate(now.getDate() + 1),
+  });
+  try {
+    let res = await axios.post("schedule/get-schedule", body, config);
+    console.log(res.data);
+    dispatch({ type: SCHEDULE_FETCHED, payload: res.data });
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (err.response.status === 401) {
+      dispatch({ type: LOGOUT });
+    }
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.message, "danger")));
+    }
+  }
+};
+
+export const createSchedule =
+  ({
+    route,
+    bus_type,
+    from: fromDO,
+    to: toDO,
+    departure,
+    arrival,
+    recurring,
+    price,
+  }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({
+      route,
+      bus_type,
+      from: fromDO,
+      to: toDO,
+      departure,
+      arrival,
+      recurring,
+      price,
+    });
+    try {
+      let res = await axios.post("schedule/create-schedule", body, config);
+      dispatch({ type: SCHEDULE_CREATED, payload: res.data });
+    } catch (err) {
+      const errors = err.response.data.errors;
+      if (err.response.status === 401) {
+        dispatch({ type: LOGOUT });
+      }
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.message, "danger")));
+      }
+    }
+  };
